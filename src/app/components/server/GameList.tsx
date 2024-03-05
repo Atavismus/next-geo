6717caed1de8dfddd97b5abb58ed94b0f4177cb7
Atavismus <soleinoirdsr@hotmail.com>
2024-03-05 23:21:44 +0100

    add basic css to GameList

--- src/app/components/server/GameList.tsx
+++ src/app/components/server/GameList.tsx
@@ -6,21 +6,26 @@ const GameList = () => {
     return variants.map((variant, i) => {
       const url: string = `/funwith/${game}/${variant.split(' ').pop()?.replace('?', '')}`;
       return (
-        <p key={i}>{variants.length > 1 ? '>' : ''}<Link href={url}>{variant}</Link></p>
+        <p key={i}>{variants.length > 1 ? '>' : ''}<Link href={url} className="bg-blue-500 hover:bg-blue-600 active:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300">{variant}</Link></p>
       );
     });
   }
-  return getAllGamesName().map((game, i) => {
-      const country = new Game({ name: game });
-      return (
-          <div key={i}>
-              <>
-                { country.getName() }:
-                { renderGameVariants(game, country.variants) }
-              </>
+  const games = getAllGamesName();
+  return (
+    <div className="grid grid-cols-2 gap-4">
+      { games.map((game, i) => {
+        const country = new Game({ name: game });
+        return (
+          <div key={i} className="border border-black p-4">
+            <>
+              { country.getName() }:
+              { renderGameVariants(game, country.variants) }
+            </>
           </div>
-      );
-  });
+        );
+      })}
+    </div>
+  );
 }
 
 export { GameList };

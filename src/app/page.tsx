943d27580696162863e542febcc46ecc56f4a279
Atavismus <soleinoirdsr@hotmail.com>
2024-03-05 11:54:54 +0100

    refacto homepage with recent adds

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
 import { ApiData } from './models/ApiData';
-import { RandomPick } from './components/RandomPick';
-
-// Fetch all countries from the countryapi.io
-const getData = async ():Promise<ApiData> => {
-  const res = await fetch(`https://countryapi.io/api/all?apikey=${process.env.API_KEY}`);
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
-  return res.json();
-}
+import { getData } from './helpers/getData';
+import { RandomPick } from './components/server/RandomPick';
+import { GameList } from './components/server/GameList';
 
 export default async function Home() {
   const apiData: ApiData = await getData();
@@ -17,5 +10,6 @@ export default async function Home() {
       <p>{Object.keys(apiData).length} countries await you!</p>
       <p>Here is a random one:</p>
       <RandomPick data={ apiData } />
+      <GameList />
     </main>);
 }

bd9c43cb444c0684cb24595a746d7e72d6801ab7
Atavismus <soleinoirdsr@hotmail.com>
2024-03-11 22:53:05 +0100

    add CheckIcon

---
+++ src/app/components/server/CheckIcon.tsx
@@ -0,0 +1,14 @@
+interface ICheckIcon {
+    className: string;
+}
+const CheckIcon = (props: ICheckIcon) => {
+    const { className } = props;
+    return(
+        <svg width="40px" height="40px" viewBox="0 0 400 400" fill="none" xmlns="http://www.w3.org/2000/svg" className={className}>
+            <path d="M341 67C148.339 290.671 183.5 343.046 131 205.023" stroke="#000000" strokeOpacity="0.9" strokeWidth="20" strokeLinecap="round"/>
+            <path d="M126.678 74.6892C175.762 40.3656 223.672 30.3765 275.706 57.7602C415.664 131.409 354.622 382.384 180.329 356.983C85.4278 343.149 -9.51278 223.621 90.9151 117.034" stroke="#000000" strokeOpacity="0.9" strokeWidth="20" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+    )
+}
+
+export { CheckIcon };

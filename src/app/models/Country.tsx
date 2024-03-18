10c2f1f11ba0f9b3113959e6e18a467feafeefb5
Atavismus <soleinoirdsr@hotmail.com>
2024-03-18 17:22:18 +0100

    update country model with latLng

--- src/app/models/Country.tsx
+++ src/app/models/Country.tsx
@@ -1,5 +1,10 @@
 import { ApiData } from "./ApiData";
 
+export interface ILocationData {
+    country?: [number, number];
+    capital?: [number, number];
+}
+
 export interface ICountry {
     area?: number;
     capital?: string;
@@ -8,6 +13,7 @@ export interface ICountry {
     population?: number;
     region?: string;
     subregion?: string;
+    latLng?: ILocationData;
 }
 
 export class Country implements ICountry  {
@@ -18,6 +24,7 @@ export class Country implements ICountry  {
     population;
     region;
     subregion;
+    latLng;
     constructor(object?: ICountry) {
         if(object) {
             this.area = object.area;
@@ -27,12 +34,19 @@ export class Country implements ICountry  {
             this.population = object.population;
             this.region = object.region;
             this.subregion = object.subregion;
+            this.latLng = object.latLng;
         }
     }
     public get<K extends keyof ICountry>(propertyName: K): ICountry[K] {
         return this[propertyName] as ICountry[K];
     }
-    public random(apiData: ApiData, index?: number | null) {
+    public getLat(): [number, number] | null {
+        return this.latLng?.country ?? null;
+    }
+    public getCapitalLat(): [number, number] | null {
+        return this.latLng?.capital ?? null;
+    }
+    public random(apiData: ApiData, index?: number | null): Country {
         const country = apiData[Object.keys(apiData)[index ?? randomCountryIndex(apiData)]];
         return new Country({...country});
     }

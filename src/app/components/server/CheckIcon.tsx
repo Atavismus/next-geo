700c77baaaf8be61c81020a3e9c67b039c57edcd
Atavismus <soleinoirdsr@hotmail.com>
2024-03-19 10:51:16 +0100

    refacto CheckIcon component

--- src/app/components/server/CheckIcon.tsx
+++ src/app/components/server/CheckIcon.tsx
@@ -1,8 +1,4 @@
-interface ICheckIcon {
-    className: string;
-}
-const CheckIcon = (props: ICheckIcon) => {
-    const { className } = props;
+const CheckIcon = ({ className }: { className?: string }) => {
     return(
         <svg width="40px" height="40px" viewBox="0 0 400 400" fill="none" xmlns="http://www.w3.org/2000/svg" className={className}>
             <path d="M341 67C148.339 290.671 183.5 343.046 131 205.023" stroke="#000000" strokeOpacity="0.9" strokeWidth="20" strokeLinecap="round"/>
